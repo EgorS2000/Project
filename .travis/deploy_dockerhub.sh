d5ec0e7dbd09fab16095062498f2c20631478933
Egor Shilkin <Egor.Shilkin@icloud.com>
2021-12-09 21:34:34 +0300

    docker-hub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
 docker build -f Dockerfile -t $NAMES:"latest" .
 docker push $NAMES
