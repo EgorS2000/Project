4807ff5804ee896938a56c04e9ebd8bad2764819
Egor Shilkin <Egor.Shilkin@icloud.com>
2021-12-09 21:01:59 +0300

    docker-hub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,12 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker-compose build -t $NAMES:"latest" .
-docker push $NAMES
+docker-compose build
+
+docker tag web $DOCKER_USER/project_web
+docker tag web $DOCKER_USER/project_nginx
+docker tag web $DOCKER_USER/project_db
+
+docker push $DOCKER_USER/project_web:"latest"
+docker push $DOCKER_USER/project_nginx:"latest"
+docker push $DOCKER_USER/project_db:"latest"
